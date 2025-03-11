aa9e1066f6b939c819d628b7ef4cb40ac3ba2642
Enes Ercan Enwar Hurmuzlu <enesalhurmuzi@gmail.com>
2025-03-12 00:33:27 +0300

    Create generate_participants.py

---
+++ generate_participants.py
@@ -0,0 +1,218 @@
+# List of participants (2023 and 2024)
+participants = [
+    {"name": "علي محمد جبار", "bio": "علي محمد جبار is a talented young scientist..."},
+    {"name": "Atheer Bassam Qasim", "bio": "Atheer Bassam Qasim has worked on..."},
+    {"name": "عبد العزيز علي محمود", "bio": "عبد العزيز علي محمود specializes in..."},
+    {"name": "Rawan Ihsan Kadhim", "bio": "Rawan Ihsan Kadhim is passionate about..."},
+    {"name": "Maryam Saad", "bio": "Maryam Saad has contributed to..."},
+    {"name": "عبدالله علي طه", "bio": "عبدالله علي طه is a rising star in..."},
+    {"name": "حَفصة جاسم موسى", "bio": "حَفصة جاسم موسى has worked on..."},
+    {"name": "مرتضى محمد عبد الرضا", "bio": "مرتضى محمد عبد الرضا specializes in..."},
+    {"name": "وضاح احمد عوده", "bio": "وضاح احمد عوده is passionate about..."},
+    {"name": "Abdullrahman Nawfal Ahmed", "bio": "Abdullrahman Nawfal Ahmed has contributed to..."},
+    {"name": "محمد حسن مهدي", "bio": "محمد حسن مهدي is a talented young scientist..."},
+    {"name": "Ameer Kazem", "bio": "Ameer Kazem has worked on..."},
+    {"name": "محمد وائل عباس", "bio": "محمد وائل عباس specializes in..."},
+    {"name": "Hala Abd Al Hameed Mahdi", "bio": "Hala Abd Al Hameed Mahdi is passionate about..."},
+    {"name": "نور عبدالزهرة مشتت", "bio": "نور عبدالزهرة مشتت has contributed to..."},
+    {"name": "Islam Raed Khudhair", "bio": "Islam Raed Khudhair is a rising star in..."},
+    {"name": "Safi Nas Haider Shakir", "bio": "Safi Nas Haider Shakir has worked on..."},
+    {"name": "Sara Salim Al Mahdi", "bio": "Sara Salim Al Mahdi specializes in..."},
+    {"name": "احمد وليد داود", "bio": "احمد وليد داود is passionate about..."},
+    {"name": "عبّاس حيدر قاسم", "bio": "عبّاس حيدر قاسم has contributed to..."},
+    {"name": "جعفر حسين عباس", "bio": "جعفر حسين عباس is a talented young scientist..."},
+    {"name": "منتظر علاء عبدالحافظ", "bio": "منتظر علاء عبدالحافظ has worked on..."},
+    {"name": "مرتضى علي حمودي", "bio": "مرتضى علي حمودي specializes in..."},
+    {"name": "Anjila Haji", "bio": "Anjila Haji is passionate about..."},
+    {"name": "يوسف عزيز", "bio": "يوسف عزيز has contributed to..."},
+    {"name": "Ahmed Ali Shalan", "bio": "Ahmed Ali Shalan is a rising star in..."},
+    {"name": "فاطمة بهجت كاظم", "bio": "فاطمة بهجت كاظم has worked on..."},
+    {"name": "Aya Hayder", "bio": "Aya Hayder specializes in..."},
+    {"name": "طه طالب رشيد", "bio": "طه طالب رشيد is passionate about..."},
+    {"name": "Abdullah Bashar Adnan", "bio": "Abdullah Bashar Adnan has contributed to..."},
+    {"name": "Ghada Hashim Abbas", "bio": "Ghada Hashim Abbas is a talented young scientist..."},
+    {"name": "زهراء اسامة", "bio": "زهراء اسامة has worked on..."},
+    {"name": "شهد ثامر خليل", "bio": "شهد ثامر خليل specializes in..."},
+    {"name": "Mohamed Ahmed Abdulalee", "bio": "Mohamed Ahmed Abdulalee is passionate about..."},
+    {"name": "Dima Mohammed Imad", "bio": "Dima Mohammed Imad has contributed to..."},
+    {"name": "همام حيدر صاحب", "bio": "همام حيدر صاحب is a rising star in..."},
+    {"name": "طيبة محمد هاشم", "bio": "طيبة محمد هاشم has worked on..."},
+    {"name": "Maryam Rasim", "bio": "Maryam Rasim specializes in..."},
+    {"name": "جنّات حامد رحيم", "bio": "جنّات حامد رحيم is passionate about..."},
+    {"name": "مصطفى فراس طارق", "bio": "مصطفى فراس طارق has contributed to..."},
+    {"name": "علي باسم حسين", "bio": "علي باسم حسين is a talented young scientist..."},
+    {"name": "علي عادل", "bio": "علي عادل has worked on..."},
+    {"name": "علي يوسف عيسى", "bio": "علي يوسف عيسى specializes in..."},
+    {"name": "Mohammed Mushtaq Hammod Abd Jarrah Alkhafaji", "bio": "Mohammed Mushtaq Hammod Abd Jarrah Alkhafaji is passionate about..."},
+    {"name": "بنين جليل اسماعيل", "bio": "بنين جليل اسماعيل has contributed to..."},
+    {"name": "Mustafa Saleh Jasim", "bio": "Mustafa Saleh Jasim is a rising star in..."},
+    {"name": "Zaid Hamed", "bio": "Zaid Hamed has worked on..."},
+    {"name": "مصطفى محمد كاظم", "bio": "مصطفى محمد كاظم specializes in..."},
+    {"name": "مريم عامر سالم", "bio": "مريم عامر سالم is passionate about..."},
+    {"name": "Muntazier Mahdi Jassim", "bio": "Muntazier Mahdi Jassim has contributed to..."},
+    {"name": "معصومه حسن والي", "bio": "معصومه حسن والي is a talented young scientist..."},
+    {"name": "قاسم عامر قاسم", "bio": "قاسم عامر قاسم has worked on..."},
+    {"name": "محمد سعد سلطان", "bio": "محمد سعد سلطان specializes in..."},
+    {"name": "يسر علي نوري", "bio": "يسر علي نوري is passionate about..."},
+    {"name": "فرح حسين مهدي", "bio": "فرح حسين مهدي has contributed to..."},
+    {"name": "حسين أحمد جاسم", "bio": "حسين أحمد جاسم is a rising star in..."},
+    {"name": "رقية عقيل محمد", "bio": "رقية عقيل محمد has worked on..."},
+    {"name": "منى زين العابدين", "bio": "منى زين العابدين specializes in..."},
+    {"name": "مهدي سلوان منذر", "bio": "مهدي سلوان منذر is passionate about..."},
+    {"name": "غاده مصطفى حميد", "bio": "غاده مصطفى حميد has contributed to..."},
+    {"name": "قدري باسم طالب", "bio": "قدري باسم طالب is a talented young scientist..."},
+    {"name": "Zahraa Muqdad Sami", "bio": "Zahraa Muqdad Sami has worked on..."},
+    {"name": "نور مهند عدنان", "bio": "نور مهند عدنان specializes in..."},
+    {"name": "حيدر عماد حسين علوان", "bio": "حيدر عماد حسين علوان is passionate about..."},
+    {"name": "امير محمد كاظم", "bio": "امير محمد كاظم has contributed to..."},
+    {"name": "Hamza Ali", "bio": "Hamza Ali is a rising star in..."},
+    {"name": "الاء حيدر ناجي", "bio": "الاء حيدر ناجي has worked on..."},
+    {"name": "علي محمد عبد الرضا", "bio": "علي محمد عبد الرضا specializes in..."},
+    {"name": "محمد جعفر محسن علوان", "bio": "محمد جعفر محسن علوان is passionate about..."},
+    {"name": "مصطفى كاظم جواد", "bio": "مصطفى كاظم جواد has contributed to..."},
+    {"name": "علي وليد عبد", "bio": "علي وليد عبد is a talented young scientist..."},
+    {"name": "Azzam Monther", "bio": "Azzam Monther has worked on..."},
+    {"name": "Dania Hassan Abbas Shamsallah", "bio": "Dania Hassan Abbas Shamsallah specializes in..."},
+    {"name": "Maria Hasan", "bio": "Maria Hasan is passionate about..."},
+    {"name": "أحمد عباس جاسم", "bio": "أحمد عباس جاسم has contributed to..."},
+    {"name": "MARYAM WAMEEDH ABDULJABBAR", "bio": "MARYAM WAMEEDH ABDULJABBAR is a rising star in..."},
+    {"name": "Moamal Ayad Alsarray", "bio": "Moamal Ayad Alsarray has worked on..."},
+    {"name": "زيد علي", "bio": "زيد علي specializes in..."},
+    {"name": "فاطمة علي عبدالحسين", "bio": "فاطمة علي عبدالحسين is passionate about..."},
+    {"name": "اثير محمد خضير عباس", "bio": "اثير محمد خضير عباس has contributed to..."},
+    {"name": "Noor Al Hassan Emad Khalaf", "bio": "Noor Al Hassan Emad Khalaf is a talented young scientist..."},
+    {"name": "Ghasaq Ehab Hasan", "bio": "Ghasaq Ehab Hasan has worked on..."},
+    {"name": "MohamedEzuldin Naser Ibrahim", "bio": "MohamedEzuldin Naser Ibrahim specializes in..."},
+    {"name": "Sami Basil Moaid", "bio": "Sami Basil Moaid is passionate about..."},
+    {"name": "آمنة محمد عبدالرحمن", "bio": "آمنة محمد عبدالرحمن has contributed to..."},
+    {"name": "موسى احمد جبار", "bio": "موسى احمد جبار is a rising star in..."},
+    {"name": "احمد عمر صبحي حميد", "bio": "احمد عمر صبحي حميد has worked on..."},
+    {"name": "عباس فؤاد مهدي", "bio": "عباس فؤاد مهدي specializes in..."},
+    {"name": "عبدالله مصطفى محمد", "bio": "عبدالله مصطفى محمد is passionate about..."},
+    {"name": "Bakr Amjed Ramadhan Al-Rawi", "bio": "Bakr Amjed Ramadhan Al-Rawi has contributed to..."},
+    {"name": "Amna Ali", "bio": "Amna Ali is a talented young scientist..."},
+    {"name": "كرار عبد الرزاق حسين", "bio": "كرار عبد الرزاق حسين has worked on..."},
+    {"name": "Mustafa Loay", "bio": "Mustafa Loay specializes in..."},
+    {"name": "سنا لؤي رؤوف", "bio": "سنا لؤي رؤوف is passionate about..."},
+    {"name": "حسين ظافر", "bio": "حسين ظافر has contributed to..."},
+    {"name": "يقين حامد", "bio": "يقين حامد is a rising star in..."},
+    {"name": "هاجر مروان مهدي", "bio": "هاجر مروان مهدي has worked on..."},
+    {"name": "صادق سرحان نوحت", "bio": "صادق سرحان نوحت specializes in..."},
+    {"name": "يوسف عُدي عبدالقادر", "bio": "يوسف عُدي عبدالقادر is passionate about..."},
+    {"name": "Mustafa Hameed Khalaf", "bio": "Mustafa Hameed Khalaf has contributed to..."},
+    {"name": "رواء عماد كاظم", "bio": "رواء عماد كاظم is a talented young scientist..."},
+    {"name": "Ahmed Firas Qais Aldoghman", "bio": "Ahmed Firas Qais Aldoghman has worked on..."},
+    {"name": "Rama Firas Qais Aldoghman", "bio": "Rama Firas Qais Aldoghman specializes in..."},
+    {"name": "تقوى حسين عبدالله", "bio": "تقوى حسين عبدالله is passionate about..."},
+    {"name": "محمد حسن ستار", "bio": "محمد حسن ستار has contributed to..."},
+    {"name": "محمد عباس عدنان", "bio": "محمد عباس عدنان is a rising star in..."},
+    {"name": "ابراهيم اديسون شكري", "bio": "ابراهيم اديسون شكري has worked on..."},
+    {"name": "عبد الودود فراس", "bio": "عبد الودود فراس specializes in..."},
+    {"name": "Eslam Mustafaa Ahmed", "bio": "Eslam Mustafaa Ahmed is passionate about..."},
+    {"name": "مروة عبد السلام عبد الرحمن", "bio": "مروة عبد السلام عبد الرحمن has contributed to..."},
+    {"name": "Fatima Ammar Malik", "bio": "Fatima Ammar Malik is a talented young scientist..."},
+    {"name": "منار شهاب احمد", "bio": "منار شهاب احمد has worked on..."},
+    {"name": "احمد محمد عبدالله علي", "bio": "احمد محمد عبدالله علي specializes in..."},
+    {"name": "زينب سعيد جمعة", "bio": "زينب سعيد جمعة is passionate about..."},
+    {"name": "سندس أمين", "bio": "سندس أمين has contributed to..."},
+    {"name": "Haneen Abdulkhader Mahmood", "bio": "Haneen Abdulkhader Mahmood is a rising star in..."},
+    {"name": "يقين حامد", "bio": "يقين حامد has worked on..."},
+    {"name": "نور الهدى احمد محمد", "bio": "نور الهدى احمد محمد specializes in..."},
+    {"name": "محمد صلاح مهدي", "bio": "محمد صلاح مهدي is passionate about..."},
+    {"name": "جعفر حسين", "bio": "جعفر حسين has contributed to..."},
+    {"name": "محمد علي سعد", "bio": "محمد علي سعد is a talented young scientist..."},
+    {"name": "ريم علي فائز النافوسي", "bio": "ريم علي فائز النافوسي has worked on..."},
+    {"name": "ملاك مهند بريسم", "bio": "ملاك مهند بريسم specializes in..."},
+    {"name": "رفقة عبدالله عدنان", "bio": "رفقة عبدالله عدنان is passionate about..."},
+    {"name": "حوراء هيثم كاظم", "bio": "حوراء هيثم كاظم has contributed to..."},
+    {"name": "Haya Shameem Salman", "bio": "Haya Shameem Salman is a rising star in..."},
+    {"name": "Mohammed Mazin", "bio": "Mohammed Mazin has worked on..."},
+    {"name": "Mohammed Walaa", "bio": "Mohammed Walaa specializes in..."},
+    {"name": "بشير محمد", "bio": "بشير محمد is passionate about..."},
+    {"name": "محمد حسين علي", "bio": "محمد حسين علي has contributed to..."},
+    {"name": "Mustafa Aqeel", "bio": "Mustafa Aqeel is a talented young scientist..."},
+    {"name": "زيد احمد عمار", "bio": "زيد احمد عمار has worked on..."},
+    {"name": "Zainab Saeed", "bio": "Zainab Saeed specializes in..."},
+    {"name": "حسنين علي محمد", "bio": "حسنين علي محمد is passionate about..."},
+    {"name": "Ghina Hussein Amouri", "bio": "Ghina Hussein Amouri has contributed to..."},
+    {"name": "علي محمد عزيز", "bio": "علي محمد عزيز is a rising star in..."},
+    {"name": "Mustafa Kadhum Jawad", "bio": "Mustafa Kadhum Jawad has worked on..."},
+    {"name": "Karar Abdul-Razzaq", "bio": "Karar Abdul-Razzaq specializes in..."},
+    {"name": "Hajir Marwan Mahdi", "bio": "Hajir Marwan Mahdi is passionate about..."},
+    {"name": "Rawaa Emad Khadim", "bio": "Rawaa Emad Khadim has contributed to..."},
+    {"name": "Mohammed Mazin", "bio": "Mohammed Mazin is a talented young scientist..."},
+    {"name": "ملاك مهند بريسم", "bio": "ملاك مهند بريسم has worked on..."},
+    {"name": "Mustafa Hameed Khalaf", "bio": "Mustafa Hameed Khalaf specializes in..."},
+    {"name": "محمد عباس عدنان", "bio": "محمد عباس عدنان is passionate about..."},
+    {"name": "Manar Shehab Ahmed", "bio": "Manar Shehab Ahmed has contributed to..."},
+    {"name": "Kawther Alobaidy", "bio": "Kawther Alobaidy is a rising star in..."},
+    {"name": "Haneen Abdulkhader Mahmood", "bio": "Haneen Abdulkhader Mahmood has worked on..."},
+    {"name": "Ameer Saheb Khalaf", "bio": "Ameer Saheb Khalaf specializes in..."},
+    {"name": "Ghina Hussein Amouri", "bio": "Ghina Hussein Amouri is passionate about..."},
+    {"name": "صادق سرحان نوحت", "bio": "صادق سرحان نوحت has contributed to..."},
+    {"name": "Taha Talib Rasheed", "bio": "Taha Talib Rasheed is a talented young scientist..."},
+    {"name": "مصطفى علي عادل", "bio": "مصطفى علي عادل has worked on..."},
+    {"name": "احمد عمر صبحي", "bio": "احمد عمر صبحي specializes in..."},
+    {"name": "Zaid Ahmed Ammar", "bio": "Zaid Ahmed Ammar is passionate about..."},
+    {"name": "Mustafa Nazar Qasim", "bio": "Mustafa Nazar Qasim has contributed to..."},
+    {"name": "Haya Shameem Salman", "bio": "Haya Shameem Salman is a rising star in..."},
+    {"name": "Mohammed Salah Mahdi", "bio": "Mohammed Salah Mahdi has worked on..."},
+]
+
+# HTML template for participant pages
+template = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{name} - Iraq Youth Scientist Team</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../images/logo.png" alt="Iraq Youth Scientist Team Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../about.html">About Us</a></li>
+                <li><a href="../programs.html">Programs</a></li>
+                <li><a href="../news.html">News</a></li>
+                <li><a href="../contact.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="participant-profile">
+        <h1>{name}</h1>
+        <img src="../images/participants/{image}" alt="{name}">
+        <div class="bio">
+            <h2>About {name}</h2>
+            <p>{bio}</p>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Iraq Youth Scientist Team. All rights reserved.</p>
+    </footer>
+
+    <script src="../js/scripts.js"></script>
+</body>
+</html>
+"""
+
+# Generate HTML files for each participant
+for participant in participants:
+    # Create a filename by replacing spaces with hyphens and converting to lowercase
+    filename = participant["name"].replace(" ", "-").lower() + ".html"
+    # Write the HTML file
+    with open(f"participants/{filename}", "w", encoding="utf-8") as file:
+        file.write(template.format(
+            name=participant["name"],
+            bio=participant["bio"],
+            image=participant["name"].replace(" ", "-").lower() + ".jpg"
+        ))
+
+print("Participant pages generated successfully!")
